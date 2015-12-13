14d7edf54e450c7fee8a7738ab3f93a377e16cdb
Giancarlo <Giancarlo@DESKTOP-COSN1P3>
2015-12-13 19:00:07 -0200

    Construção do módulo Fornecedor.

---
+++ SGPOModelos/src/main/java/br/com/sgpo/administrativo/modelo/Fornecedor.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sgpo.administrativo.modelo;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ *
+ * @author Giancarlo
+ */
+@Entity
+@Table(name = "fornecedor", schema = "administrativo")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Fornecedor implements Serializable {
+
+    @Id
+    @Column(name = "for_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotBlank
+    @Column(name = "for_nome", nullable = false, unique = true)
+    private String nome;
+    
+    @Email
+    @Column(name = "for_email")
+    private String email;
+    
+    @Column(name = "for_telefone")
+    private String telefone;
+    
+    @Column(name = "for_celular")
+    private String celular;
+    
+    @ManyToOne
+    @JoinColumn(name = "end_id", referencedColumnName = "end_id")
+    private Endereco endereço;
+    
+    @Column(name = "for_representante")
+    private String representante;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public Endereco getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(Endereco endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getRepresentante() {
+        return representante;
+    }
+
+    public void setRepresentante(String representante) {
+        this.representante = representante;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Fornecedor other = (Fornecedor) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+}
