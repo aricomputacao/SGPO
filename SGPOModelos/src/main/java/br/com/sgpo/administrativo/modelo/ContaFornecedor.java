fcec4bf893c359d453dce3b4a87f24d583a38f83
ari <ari@ari-Inspiron-N4050>
2016-01-26 01:31:10 -0300

    conclusao emprestimo colaborador

---
+++ SGPOModelos/src/main/java/br/com/sgpo/administrativo/modelo/ContaFornecedor.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sgpo.administrativo.modelo;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * @author ari
+ */
+@Entity
+@Table(schema = "administrativo", name = "conta_fornecedor")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class ContaFornecedor implements Serializable{
+    
+    @Id
+    @Column(name = "cof_id",nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotEmpty
+    @Column(name = "con_agencia",nullable = false)
+    private String agencia;
+    
+    @NotEmpty
+    @Column(name = "con_numero",nullable = false)
+    private String numero;
+    
+    @NotEmpty
+    @Column(name = "con_banco",nullable = false)
+    private String banco;
+    
+    @NotNull
+    @ManyToOne
+    @JoinColumn(columnDefinition = "for_id",referencedColumnName = "for_id",nullable = false)
+    private Fornecedor fornecedor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ContaFornecedor other = (ContaFornecedor) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+    
+}
